8660f264c30693c28921e6d7e2012b1d37691242
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-20 15:01:45 +0000

    Finished vectorized version of the gradient

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -73,8 +73,8 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
-                dW[:,j] += X[i].T
-                dW[:,y[i]] -= X[i].T
+                dW[:,j] += X[i]
+                dW[:,y[i]] -= X[i]
                 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -109,7 +109,16 @@ def svm_loss_vectorized(W, X, y, reg):
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     pass
-
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    scores = X.dot(W)
+    correct_class_score = scores[np.arange(num_train),y].reshape(num_train,1)
+    margin = np.maximum(0,scores - correct_class_score + 1)
+    margin[np.arange(num_train),y] = 0 #NOTE: delta of the correct position should be set 0
+    loss = np.sum(margin)
+    loss /= num_train
+    loss += reg * np.sum(W * W)
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     #############################################################################
@@ -124,6 +133,9 @@ def svm_loss_vectorized(W, X, y, reg):
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     pass
+    margin = (margin > 0).astype(int)
+    margin[np.arange(num_train),y] -= np.sum(margin,axis = 1)
+    dW = np.dot(X.T,margin) / num_train + 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
